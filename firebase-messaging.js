ee03ff392efa101a007890f5e01d4358ffdb4afa
WETrendingTeam <lade.galleria@gmail.com>
2026-08-06 12:05:49 +0100

    Add files via upload

---
+++ firebase-messaging.js
@@ -0,0 +1,33 @@
+import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+import { app } from "./firebase-config.js";
+
+const messaging = getMessaging(app);
+
+export async function requestNotificationPermission() {
+
+  const permission = await Notification.requestPermission();
+
+  if (permission === "granted") {
+
+    console.log("Notification permission granted");
+
+    const token = await getToken(messaging, {
+      vapidKey: "BAw-ZA0fgoyK9bcUVcbZOeI_0oRNJZBtSkxRcPDJGba3nXRdo27FV4L0qHTZd_K7z_RE3qCToyK34gwMivNNBdE"
+    });
+
+    console.log("FCM Token:", token);
+
+  } else {
+
+    console.log("Notification permission denied");
+
+  }
+
+}
+
+
+onMessage(messaging, (payload) => {
+
+  console.log("New notification:", payload);
+
+});
--- service-worker.js
+++ service-worker.js
@@ -16,9 +16,60 @@ self.addEventListener("install", event => {
   );
 });
 
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
     .then(response => response || fetch(event.request))
   );
 });
+
+
+// Firebase Messaging
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"
+);
+
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js"
+);
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDY5F84tiRyDLNPBaBGpO5giwxlJ4q27Cg",
+  authDomain: "wetrendingteam-1f8ce.firebaseapp.com",
+  projectId: "wetrendingteam-1f8ce",
+  storageBucket: "wetrendingteam-1f8ce.firebasestorage.app",
+  messagingSenderId: "1072737815830",
+  appId: "1:1072737815830:web:4fce8aa6e88680404e1437"
+});
+
+
+const messaging = firebase.messaging();
+
+
+messaging.onBackgroundMessage((payload) => {
+
+  console.log(
+    "Background message received:",
+    payload
+  );
+
+
+  const notificationTitle =
+    payload.notification.title;
+
+
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/images/logo.png"
+  };
+
+
+  self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+
+});
+
