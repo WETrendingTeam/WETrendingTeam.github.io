3b53c80155bf38ccf839c7f46907bfe977564396
WETrendingTeam <lade.galleria@gmail.com>
2026-08-06 12:43:15 +0100

    Add files via upload

--- firebase-messaging.js
+++ firebase-messaging.js
@@ -3,19 +3,26 @@ import { app } from "./firebase-config.js";
 
 const messaging = getMessaging(app);
 
+
 export async function requestNotificationPermission() {
-alert("Button connected");
+
   const permission = await Notification.requestPermission();
+
 
   if (permission === "granted") {
 
     console.log("Notification permission granted");
 
+
     const token = await getToken(messaging, {
       vapidKey: "BAw-ZA0fgoyK9bcUVcbZOeI_0oRNJZBtSkxRcPDJGba3nXRdo27FV4L0qHTZd_K7z_RE3qCToyK34gwMivNNBdE"
     });
 
+
+    alert("Device connected to WETrendingTeam notifications");
+
     console.log("FCM Token:", token);
+
 
   } else {
 
