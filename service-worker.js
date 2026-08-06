29c6402e0d62408ac12ea65ad76a39439ce6de8c
WETrendingTeam <lade.galleria@gmail.com>
2026-08-06 10:16:08 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "wetrending-v1";
+
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./campaign.html",
+  "./trend.html",
+  "./hub.css",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
