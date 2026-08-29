786fe1d54758f2babb34fbfeeda8361ccb5f51d7
WETrendingTeam <lade.galleria@gmail.com>
2026-08-30 00:50:23 +0100

    Delete Firebase directory

--- Firebase/firebase-config.js
+++
@@ -1,3 +0,0 @@
-// Compatibility entry point for older BALAD files.
-// The live Firebase configuration is maintained in ../firebase-config.js.
-export { app, db, auth, getBaladMessaging } from "../firebase-config.js";
